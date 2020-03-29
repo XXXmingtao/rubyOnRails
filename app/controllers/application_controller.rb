9f5aaf52b0548116c9b78030d732a65876434a9e
Mingtao Liu <s3714710@student.rmit.edu.au>
2020-03-29 21:42:34 +1100

    solve bundle issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    def cart
+        render html: "hello, World!"
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   get 'cart' => 'cart#index'
+  get 'static_pages/about'
+  get 'cart/index'
+  root 'application#cart'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
